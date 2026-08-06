719d441da069c2e173f98fc09492f9b8cf3b5d75
ghaidasameer17-dot <ghaidasameer17@gmail.com>
2026-08-07 01:21:50 +0300

    تحديث الكود

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,10 +1,15 @@
 import'./Login.css';
 const Login = () => {
   return (<div className="login-container">
+   
     <div className="login-header">
  <h2>تسجيل الدخول </h2>
     <p>سجل دخولك لمتابعة مهامك</p>
     </div>
+    <div className='error-banner'>
+      <span>⚠</span>
+      <p>البريد الإلكتروني أو كلمة المرور غير صحيحة</p>
+   </div>
    
 <div className="form-group">
  <label htmlFor="email">البريد الالكتروني</label>
